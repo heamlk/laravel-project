da8925ed1a952369940c7e68c2f037a09edd9926
heamlk <mehdiabderrazak60@gmail.com>
2025-08-30 14:01:55 -0300

    feat: validation and datails

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'username' => ['required', 'min:3', 'max:20', 'alpha_dash'],
-            'email' => ['required', 'email', 'max:50'],
-            'password' => ['required', 'min:6', 'max:50'],
+            'username' => ['required', 'min:3', 'max:20', 'alpha_dash', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:50', 'confirmed'],
         ]);
         User::create($incomingFields);
         return 'Registration logic will go here';
