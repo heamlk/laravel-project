5bab5cc66bc71e4d3bc936b5f146f00f11d7f978
heamlk <mehdiabderrazak60@gmail.com>
2025-09-22 15:37:26 -0300

    feat: what is middleware

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 // User related routes
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
-Route::get('/', [UserController::class, 'showCorrectHomepage']);
+Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
 
 // Blog related routes
-Route::get('/create-post', [PostController::class, 'createPostForm']);
-Route::post('/create-post', [PostController::class, 'submitNewPost']);
+Route::get('/create-post', [PostController::class, 'createPostForm'])->middleware('mustBeLoggedIn');;
+Route::post('/create-post', [PostController::class, 'submitNewPost'])->middleware('mustBeLoggedIn');;
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
