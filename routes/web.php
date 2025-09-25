daa5286fb207b52791a7f96538924757e478a6e7
heamlk <mehdiabderrazak60@gmail.com>
2025-09-25 13:16:20 -0300

    feat: admin only pages - what is a gate

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get('/admin', function () {
+    return 'Admin page - only for admins';
+})->middleware('can:viewAdminPage');
+
 // User related routes
 Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
 Route::post('/register', [UserController::class, 'register'])->middleware('guest');
