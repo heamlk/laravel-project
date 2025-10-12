618e737e9845f6177b539f1a77f1258ad33f6f34
heamlk <mehdiabderrazak60@gmail.com>
2025-10-12 18:56:52 -0300

    feat: live result from livewire

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,10 @@
 <div>
-    <p>Hello LiveWire!</p>
+    <input type="text" wire:model.live="searchTerm">
+    @if (count($results) > 0)
+        <ul>
+            @foreach ($results as $post)
+                <li>{{ $post->title }}</li>
+            @endforeach
+        </ul>
+    @endif
 </div>
